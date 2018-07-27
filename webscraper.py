7ffb8099fe2646d93fcbe97500bd7967db2e702b
Jacob Arriola <jarriola2012@gmail.com>
2018-07-27 14:00:50 -0700

    Updated webscraper.py with preliminary team updates

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,30 @@
 from bs4 import BeautifulSoup
 import itertools
-
-
 import requests
+
+
+# Checks for players listed on user's team in rotoworld headlines. If players match the story is updated to team.txt
+def check_team(team, players, player_stories):
+	for y,z in zip(players,player_stories):
+		for x in team: 
+			if x == y[0].get_text(strip=True):
+				with open("team.txt","r+") as f:
+					lines = f.readlines()
+					lines = [i.strip() for i in lines]
+					lines = filter(None, lines)
+
+					if z.get_text(strip=True) in lines:
+						continue
+					f.write(y[0].get_text(strip=True) + ' \r\n' + z.get_text(strip=True) + '\r\n\r\n')
+				f.close()
+
+	run_page(players, player_stories)
+
+
+def run_page(players, player_stories):
+	for x,y in zip(players, player_stories):
+		print (x[0].get_text() + '  -  ' + x[1].get_text() + ' ' + y.get_text())
+
 
 page_link ='http://www.rotoworld.com/playernews/nfl/football-player-news'
 
@@ -19,10 +41,8 @@ players_general = page_content.select('.pb .player')
 player_stories = page_content.select('.pb .report')
 players= [pg.select('a') for pg in players_general]
 
-for x,y in zip(players, player_stories):
-	print (x[0].get_text() + '  -  ' + x[1].get_text() + y.get_text())
-
-	
 
 
+team = ['Chris Carson', 'Mike Purcell']
 
+check_team(team, players, player_stories)
